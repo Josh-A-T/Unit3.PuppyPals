a60dbf7c587958b85069ed3a2b742fabd99efec0
Josh <889+Josh-A-T@users.noreply.github.com>
2025-02-05 23:36:09 -0600

    updating
    update to match my contact list app since I think it looks nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { catList } from "./data.js";
 import { useState } from "react";
-import "./App.css";
+import "./stylesheet.css";
 
-function App() {
+export default function App() {
   const [kitties, setKitties] = useState(catList);
   const [featCatId, setFeatCatId] = useState(null);
 
@@ -11,34 +11,36 @@ function App() {
   };
   const featuredCat = kitties.find((cat) => cat.id === featCatId);
   return (
-    <div className="App">
-      {kitties.map((cat) => (
-        <p onClick={() => handleClick(cat.id)} key={cat.id}>
-          {cat.name}
-        </p>
-      ))}
-      {featCatId && featuredCat && (
-        <div>
-          <h1>{featuredCat.name}</h1>
-          <img
-            src={featuredCat.image || "https://placehold.co/150"} // Default image
-            alt={`${featuredCat.name}'s picture`}
-            style={{ width: "150px", height: "150px", objectFit: "cover" }}
-          />
-          <ul>
-            <li>Age: {featuredCat.age}</li>
-            <li>Email: {featuredCat.email}</li>
-            Tricks:
+    <div className="app-container">
+      <div className="sidebar">
+        {kitties.map((cat) => (
+          <p onClick={() => handleClick(cat.id)} key={cat.id}>
+            {cat.name}
+          </p>
+        ))}
+      </div>
+
+      <div className="main-content">
+        {featCatId && featuredCat && (
+          <div className="content-card">
+            <h1>{featuredCat.name}</h1>
+            <img
+              src={featuredCat.image || "https://placehold.co/300"} // Default image
+              alt={`${featuredCat.name}'s picture`}
+            />
             <ul>
-              {featuredCat.tricks.map((trick) => (
-                <li key={trick.id}>{trick.title}</li>
-              ))}
+              <li>Age: {featuredCat.age}</li>
+              <li>Email: {featuredCat.email}</li>
+              Tricks:
+              <ul>
+                {featuredCat.tricks.map((trick) => (
+                  <li key={trick.id}>{trick.title}</li>
+                ))}
+              </ul>
             </ul>
-          </ul>
-        </div>
-      )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
